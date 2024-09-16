62aa9199fdefac869164506a08562cba7256a615
Anish Jha <107989752+Anish-Jha@users.noreply.github.com>
2024-09-16 20:57:47 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 let map= document.getElementById("gmap_canvas");
 function subMit(){ 
     let city=document.getElementById("citY").value;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=78e7e43bb31b090bb5ba44efc9cb3758`).then((apiData)=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=78e7e43bb31b090bb5ba44efc9cb3758`).then((apiData)=>{
         return apiData.json();
         }).then((daTa)=>{
             console.log(daTa);
